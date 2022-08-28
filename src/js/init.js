13617adafacac28ed1c773a835b52f63ebcbe02f
eiletan <nathanfhlai@gmail.com>
2022-08-27 19:05:53 -0700

    Add assets

---
+++ src/js/init.js
@@ -0,0 +1,13 @@
+// A collection of functions used to initalize data stored in files into the application.
+const fs = require('fs');
+const path = require('path');
+
+
+/**
+ * Initializes team directory as a JSON object by combining data from
+ * a local json file and the NHL API
+ */
+function initTeams() {
+    let pathToFile = path.resolve(__dirname + "src/assets/");
+    fs.readFile();   
+}
