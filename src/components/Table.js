145b8fb04ca26ca9a369a38ce0732b0066fb6864
eiletan <45449124+eiletan@users.noreply.github.com>
2022-09-14 15:31:06 -0700

    add error boundary component and error handling for table.js

--- src/components/Table.js
+++ src/components/Table.js
@@ -2,12 +2,28 @@ import React from 'react';
 import '../css/Table.css';
 
 export default function Table(props) {
-    // Pass data for the table in the form of an array of objects, with each object representing a table row. The prop is "data"
+    // Pass data for the table in the form of an array of objects, with each object representing a table row. The prop is "rows"
     // Pass the order in which the data should go in the table row as an array, with the list of keys in order. The prop is "dataRowOrder"
     // Pass the key name that will be used as table row key. The prop is "dataRowKey". If unspecified, then the index will be used
     // Pass the key name that will be used as table data cell key. The prop is "dataCellKey". If unspecified, then the index will be used 
 
     function createRowsAndData(arr, order, rowKey = null, cellKey = null) {
+        // Check if array of data and order keys match
+        let arrKeys = Object.keys(arr[0]);
+        let orderKeys = Object.keys(order);
+        if (arrKeys.length != orderKeys.length) {
+            throw new Error(`The number of keys for the column must be the same as the number of keys in the order array. 
+            ${arrKeys.length} is not equal to ${orderKeys.length}`);
+        }
+        arrKeys.sort();
+        orderKeys.sort();
+        // Check if the keys are the same
+        for (let a = 0; a < arrKeys.length; a++) {
+            if (arrKeys[a] != orderKeys[a]) {
+                throw new Error(`Column names are not the same: ${arrKeys[a]} is not equal to ${orderKeys[a]}`);
+            }
+        }
+
         let rows = [];
         arr.map((rowDataObj, index) => {
             let cells = [];
