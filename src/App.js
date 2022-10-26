8c780f5c97243548d47aa0fca7b70021b18525ad
eiletan <45449124+eiletan@users.noreply.github.com>
2022-10-26 16:01:25 -0700

    Start games component

--- src/App.js
+++ src/App.js
@@ -4,13 +4,17 @@ import ErrorBoundary from './components/ErrorBoundary';
 import Table from './components/Table';
 
 function App() {
+  const map1 = new Map([["col1", <img src={require("./assets/logos/Boston Bruins.png")}></img>]
+                        ,["col2", "BOS"], ["col3", "@"], ["col4", "VAN"]
+                        ,["col5", <img src={require("./assets/logos/Vancouver Canucks.png")}></img>]
+                        ,["col6", "7:00PM"]
+                      ,["ROWKEYIDENTIFIER", "9-2 LOL"]]);
   return (
     <div className="App">
       <ErrorBoundary>
         <Table 
-          rows={[new Map([["col1", "VAN"],["col2", <img src={require("./assets/logos/Vancouver Canucks.png")}></img>],["ROWKEYIDENTIFIER", 13454]]), 
-          new Map([["col1", "BOS"],["col2", <img src={require("./assets/logos/Boston Bruins.png")}></img>],["ROWKEYIDENTIFIER", null]])]}
-          classNames={["class1","class2"]}
+          rows={[map1,map1]}
+          classNames={["class1","class1","class1","class1","class1","class1"]}
           />
       </ErrorBoundary>
     </div>
--- src/components/Games.js
+++ src/components/Games.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Table from './Table';
+// Component to display a list of games for the given day
 
 export default function Games(props) {
     return (
