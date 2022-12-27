9aa56d635145a91f525624d1dd78151046f0ba1a
eiletan <nathanfhlai@gmail.com>
2022-12-26 20:40:53 -0800

    Implementing scoreboard component

--- frontend/src/components/Scoreboard.js
+++ frontend/src/components/Scoreboard.js
@@ -2,5 +2,41 @@ import React from 'react';
 import Table from './Table';
 
 export default function Scoreboard(props) {
-    return null;
+    const scoreboardStatusClassNames = ["gameScoreBoardStatusInfo awayTeamShotsOnGoal", "gameScoreBoardStatusInfo awayTeamStrength",
+                                    "gameScoreBoardStatusInfo gameTime", "gameScoreBoardStatusInfo gamePeriod",
+                                    "gameScoreBoardStatusInfo awayTeamStrength", "gameScoreBoardStatusInfo homeTeamShotsOnGoal"];
+    const scoreboardClassNames = ["gameScoreBoardInfo teamScoreboardLogo awayTeamScoreboardLogo", "gameScoreBoardInfo awayTeamScoreboardAbbr",
+                                  "gameScoreBoardInfo awayTeamScoreboardGoals", "gameScoreBoardInfo homeTeamScoreboardGoals",
+                                  "gameScoreBoardInfo awayTeamScoreboardAbbr", "gameScoreBoardInfo teamScoreboardLogo awayTeamScoreboardLogo"];
+    
+    function processGameDataForScoreboard(gameData) {
+        let gameState = gameData["currentState"];
+
+        let away = gameData["away"]["abbreviation"];
+        let awayColor = gameData["away"]["color"];
+        let awayGoals = gameState["away"]["goals"];
+        let awayLogo = gameData["away"]["logo"];
+
+        let home = gameData["home"]["abbreviation"];
+        let homeColor = gameData["home"]["color"];
+        let homeGoals = gameState["home"]["goals"];
+        let homeLogo = gameData["home"]["logo"];
+
+        let arrMap = [];
+        arrMap.push(["awayLogo",<img src={require('../assets/logos/' + awayLogo)}></img>]);
+        // arrMap.push(["awayAbbr", ""]);
+        
+    }
+
+    return (
+        <React.Fragment>
+            <Table>
+
+            </Table>
+            <Table>
+
+            </Table>
+        </React.Fragment>
+        
+    );
 }
