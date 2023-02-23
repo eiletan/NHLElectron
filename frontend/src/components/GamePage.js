bd6d8e34eec4e4f963cd1003ea14fdf316e6911d
eiletan <nathanfhlai@gmail.com>
2023-02-23 15:47:32 -0800

    Fix issue with game update interval

--- frontend/src/components/GamePage.js
+++ frontend/src/components/GamePage.js
@@ -1,4 +1,4 @@
-import {React, useEffect, useState} from 'react';
+import {React, useEffect, useState, useRef} from 'react';
 import Scoreboard from './Scoreboard';
 import Goals from './Goals';
 import axios, * as others from 'axios';
@@ -9,12 +9,12 @@ export default function GamePage(props) {
     let {id} = useParams();
     const [gameData, setGameData] = useState(null);
     const [errorMessage, setErrorMessage] = useState(null);
+    const intervalRef = useRef();
 
     // On component mount, if props.gameData is not defined, call the API and store the result in local storage
     // .. before rendering it in the component
     useEffect(() => {
         setErrorMessage(null);
-        let gameInterval;
         if (!props.gameData) {
             // apiBase is set in local storage from the App component
             let apiBase = window.localStorage.getItem("apiBase");
@@ -32,34 +32,40 @@ export default function GamePage(props) {
             setGameData(props.gameData);
         }
 
-        if (!gameInterval) {
-            gameInterval = setInterval(getGameUpdate,60000);
-            console.log("printing");
-            console.log(gameInterval);
+        if (!intervalRef.current) {
+            intervalRef.current = setInterval(getGameUpdate,60000);
+
         }
 
         return function cleanup() {
-            console.log("clearing");
-            console.log(gameInterval);
-            clearInterval(gameInterval);
+            stopInterval();
         }
 
     },[]);
 
     useEffect(() => {
         window.localStorage.setItem('activeGameData',JSON.stringify(gameData));
+        if (gameData?.["currentState"]?.["periodTimeRemaining"] == "Final") {
+            stopInterval();
+        }
     },[gameData]);
 
 
     function getGameUpdate(gameData) {
         let apiBase = window.localStorage.getItem("apiBase");
         axios.get(apiBase+"/gameUpdate").then((response) => {
-            console.log("print interval");
             console.log(response.data);
             setGameData(response.data);
         }).catch((err) => {
             setErrorMessage(err);
         });
+    }
+
+    function stopInterval() {
+        if (intervalRef.current) {
+            clearInterval(intervalRef.current);
+            intervalRef.current = null;
+        }
     }
 
 
