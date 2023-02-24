eac564b53880475ef4eb1818fcf72fda0b8b367a
eiletan <nathanfhlai@gmail.com>
2023-02-23 16:43:22 -0800

    ui changes

--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -4,7 +4,7 @@ import Games from './Games';
 export default function Homepage(props) {
     return (
         <div className="homePageContainer">
-            <p className="componentText title">NHL APP</p>
+            <p className="componentText banner title">NHL APP</p>
             <Games gamesData={props.gamesData} date={props.date} internalTeams={props.internalTeams} onClickHandler={props.onClickHandler} onHoverHandler={props.onHoverHandler}></Games>
         </div>
     );
